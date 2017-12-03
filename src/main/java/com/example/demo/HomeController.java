9f043c1d35e03ac1d4ea24921c97723025c55d0e
Wesley Bond <wesley.bond@icloud.com>
2017-12-03 01:42:56 -0500

    m

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//
+//@Controller
+//
+//public class HomeController {
+//    @GetMapping("/songform")
+//    public String loadFormPage(Model model){
+//        model.addAttribute("song", new Song());
+//        return "songform";
+//    }
+//    @PostMapping("/songform")
+//    public String loadFromPage(@ModelAttribute Song song, Model model){
+//        model.addAttribute("song", song);
+//        return "confirmsong";
+//    }
+//}
+
+// For some reason this page is not working
---
+++ src/main/java/com/example/demo/Song.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Song {
+    private long id;
+    private String name;
+    private String artist;
+    private String album;
+    private int rating;
+    private int year;
+}
